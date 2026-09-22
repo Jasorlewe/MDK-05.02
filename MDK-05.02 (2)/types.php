7bbf1a97a1a746271a91dae104d3ac94b3f83486
YaroslavZam <yazamyatkin@gmail.com>
2026-09-22 11:39:25 +0300

    types.php

--- MDK-05.02 (2)/types.php
+++ MDK-05.02 (2)/types.php
@@ -1,6 +1,14 @@
 <h1>типы данных</h1>
 <h1>int - целые числа</h1>
 <?php
-$number=478;
-echo $number;
+$number=-478;
+$numb=0x3E;
+echo "$number, $numb";
+?>
+<h2>float - числа с плавающей точкой</h2>
+<?php
+$a=1.2;
+$b=3.4e5; //340000
+$c=5e-3; //0.005
+echo "a=$a, b=$b, c=$c";
 ?>
